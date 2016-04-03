1dfbb42e0b1456753a7f9fdee864721395bafca5
Joao De Melo <jmelo@lyncode.com>
2016-04-03 22:48:53 +0100

    Upping jtwig-core version

--- src/main/java/org/jtwig/spaceless/node/SpacelessNode.java
+++ src/main/java/org/jtwig/spaceless/node/SpacelessNode.java
@@ -1,25 +1,11 @@
 package org.jtwig.spaceless.node;
 
-import org.jtwig.context.RenderContext;
-import org.jtwig.context.model.EscapeMode;
 import org.jtwig.model.position.Position;
 import org.jtwig.model.tree.ContentNode;
 import org.jtwig.model.tree.Node;
-import org.jtwig.render.Renderable;
-import org.jtwig.render.StringBuilderRenderResult;
-import org.jtwig.render.impl.StringRenderable;
-import org.jtwig.spaceless.configuration.SpacelessConfiguration;
 
 public class SpacelessNode extends ContentNode {
     public SpacelessNode(Position position, Node content) {
         super(position, content);
-    }
-
-    @Override
-    public Renderable render(RenderContext context) {
-        StringBuilderRenderResult result = new StringBuilderRenderResult();
-        String content = super.render(context).appendTo(result).content();
-        String contentWithoutSpaces = SpacelessConfiguration.spaceRemover(context.environment()).removeSpaces(content);
-        return new StringRenderable(contentWithoutSpaces, EscapeMode.NONE);
     }
 }
---
+++ src/main/java/org/jtwig/spaceless/render/node/SpacelessNodeRender.java
@@ -0,0 +1,21 @@
+package org.jtwig.spaceless.render.node;
+
+import org.jtwig.render.RenderRequest;
+import org.jtwig.render.context.model.EscapeMode;
+import org.jtwig.render.node.renderer.NodeRender;
+import org.jtwig.renderable.Renderable;
+import org.jtwig.renderable.StringBuilderRenderResult;
+import org.jtwig.renderable.impl.StringRenderable;
+import org.jtwig.spaceless.configuration.SpacelessConfiguration;
+import org.jtwig.spaceless.node.SpacelessNode;
+
+public class SpacelessNodeRender implements NodeRender<SpacelessNode> {
+    @Override
+    public Renderable render(RenderRequest request, SpacelessNode node) {
+        StringBuilderRenderResult result = new StringBuilderRenderResult();
+        Renderable renderable = request.getEnvironment().getRenderEnvironment().getRenderNodeService().render(request, node.getContent());
+        String content = renderable.appendTo(result).content();
+        String contentWithoutSpaces = SpacelessConfiguration.spaceRemover(request.getEnvironment()).removeSpaces(content);
+        return new StringRenderable(contentWithoutSpaces, EscapeMode.NONE);
+    }
+}
